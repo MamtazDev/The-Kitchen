27fa5cfb07bdbcd279d30ea23ad02139c109c64e
tonni-akter-dev <tanniakhter123@gmail.com>
2023-09-15 23:07:11 +0600

    marquee add

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
 import NavBar from "../Shared/Navbar/Navbar";
+import About from "../Shared/Navbar/About";
 
 const Layout = () => {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar /> */}
+      <About/>
       <Outlet />
       <Footer />
     </div>
---
+++ src/Shared/Navbar/About.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-sm-12">
+            <h1>Mission</h1>
+          </div>
+          <div className="col-lg-6 col-sm-12"></div>
+        </div>
+      </div>
+      <div className="tech-slideshow marquee">
+        <div className="mover-1 marque-container">
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+          <span>VALUES</span>
+          <img
+            src="https://d2ozwg77fjfk77.cloudfront.net/636/studio/assets/v1665637651212_805549379/hashtag_smile%2019.png"
+            alt="hashtag smile"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
